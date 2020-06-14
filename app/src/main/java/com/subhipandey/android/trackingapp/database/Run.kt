b349477a21590720224b4baf46cca5aea64c303e
Subhi Pandey <subhi2012cpi@gmail.com>
2020-06-14 21:10:47 +0530

    Created Room Database

---
+++ app/src/main/java/com/subhipandey/android/trackingapp/database/Run.kt
@@ -0,0 +1,20 @@
+package com.subhipandey.android.trackingapp.database
+
+import android.graphics.Bitmap
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "running_table")
+data class Run(
+    var img: Bitmap? = null,
+    var timestamp: Long = 0L,
+    var avgSpeedInKMH: Float = 0f,
+    var distanceInMeters: Int = 0,
+    var timeInMillisecond: Long = 0L,
+    var caloriesBurned: Int = 0
+
+    ) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int? = null
+
+}
---
+++ app/src/main/java/com/subhipandey/android/trackingapp/database/RunDao.kt
@@ -0,0 +1,40 @@
+package com.subhipandey.android.trackingapp.database
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface RunDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(run: Run)
+
+    @Delete
+    suspend fun deleteRun(run: Run)
+
+    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
+    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY timeInMillisecond DESC")
+    fun getAllRunsSortedByTimeMillis(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY caloriesBurned DESC")
+    fun getAllRunsSortedByCaloriesBurned(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY avgSpeedInKMH  DESC")
+    fun getAllRunsSortedByAvgSpeed(): LiveData<List<Run>>
+
+    @Query("SELECT * FROM running_table ORDER BY distanceInMeters DESC")
+    fun getAllRunsSortedByDistance(): LiveData<List<Run>>
+
+    @Query("SELECT SUM(timeInMillisecond) FROM running_table")
+    fun getTotalTimeInMillis(): LiveData<Long>
+
+    @Query("SELECT SUM(caloriesBurned) FROM running_table")
+    fun getTotalCaloriesBurned(): LiveData<Int>
+
+    @Query("SELECT SUM(distanceInMeters) FROM running_table")
+    fun getTotalDistance(): LiveData<Int>
+
+    @Query("SELECT SUM(avgSpeedInKMH) FROM running_table")
+    fun getTotalAvgSpeed(): LiveData<Float>
+}
