0652d0691e7ba56438518328fdde412b9927b3ba
Subhi Pandey <subhi2012cpi@gmail.com>
2020-06-20 19:40:27 +0530

    DI to MainActivity

--- app/src/main/java/com/subhipandey/android/trackingapp/MainActivity.kt
+++ app/src/main/java/com/subhipandey/android/trackingapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.subhipandey.android.trackingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.subhipandey.android.trackingapp.database.RunDao
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDao: RunDao
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("runDao", "RUNDAO: ${runDao.hashCode()}")
     }
 }
--- app/src/main/java/com/subhipandey/android/trackingapp/di/ApplicationModule.kt
+++ app/src/main/java/com/subhipandey/android/trackingapp/di/ApplicationModule.kt
@@ -5,19 +5,27 @@ import androidx.room.Room
 import com.subhipandey.android.trackingapp.database.RunningDatabase
 import com.subhipandey.android.trackingapp.util.Constants.RUNNING_DATABASE_NAME
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 @Module
 @InstallIn(ApplicationComponent::class)
 object ApplicationModule {
 
+    @Singleton
+    @Provides
     fun provideRunningDatabase(
         @ApplicationContext app: Context
     ) = Room.databaseBuilder(
         app,
         RunningDatabase::class.java,
         RUNNING_DATABASE_NAME
-    )
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideRunDao(db: RunningDatabase) = db.getRunDao()
 }
