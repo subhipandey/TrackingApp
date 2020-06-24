dc240c69602968d9996dafad0c4bb183c07bea53
Subhi Pandey <subhi2012cpi@gmail.com>
2020-06-25 02:19:44 +0530

    Setup Navigation

--- app/src/main/java/com/subhipandey/android/trackingapp/ui/fragments/SetupFragment.kt
+++ app/src/main/java/com/subhipandey/android/trackingapp/ui/fragments/SetupFragment.kt
@@ -1,7 +1,18 @@
 package com.subhipandey.android.trackingapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.subhipandey.android.trackingapp.R
+import kotlinx.android.synthetic.main.fragment_setup.*
 
 class SetupFragment : Fragment(R.layout.fragment_setup) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvContinue.setOnClickListener{
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+    }
 }
